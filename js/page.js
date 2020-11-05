183b063f953c2eab68cc782f84f73e3983a5e4e8
dani-g328 <70335247+dani-g328@users.noreply.github.com>
2020-11-05 12:30:58 -0500

    base work

--- js/page.js
+++ js/page.js
@@ -1,1 +1,38 @@
-
+funcion navToggle() {
+  var x =document.getElementById("main-nav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className ="topnav";
+  }
+}
+var slideIndex =1;
+showSlides (slideIndex = n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var indicator = document.getElementsByClassName("indicator");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex =slides.length}
+  for (i=0; i < slides.length; i++) {
+    indicator[i].className = indicator[i].className.replace("active","");
+  }
+  slides[slideIndex-1].style.display = "block";
+  indicator[slideIndex-1].className += "active";
+}
+$(".button").on("click", function() {
+  var modal =$(this).data("modal");
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "Modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
